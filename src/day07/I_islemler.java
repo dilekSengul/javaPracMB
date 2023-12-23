3e993267ad46026e68f79d805af5dcfb1d6018a8
babayigit <babayigit@gmail.com>
2023-12-23 23:52:40 +0300

    Java Practice T130. End Of JP

---
+++ src/day07/I_islemler.java
@@ -0,0 +1,18 @@
+package day07;
+
+public interface I_islemler {
+    /*
+         1-EKLEME
+		 2-ARAMA
+		 3-LİSTELEME
+		 4-SİLME
+		 5-ANA MENÜ
+		 Q-ÇIKIŞ
+     */
+    void ekleme();
+    void arama();
+    void listeleme();
+    void silme();
+    void anaMenu();
+    void cikis();
+}
---
+++ src/day07/Runner.java
@@ -0,0 +1,8 @@
+package day07;
+
+public class Runner {
+    public static void main(String[] args) {
+        AnaMenu anaMenu=new AnaMenu();
+        anaMenu.menu();
+    }
+}
